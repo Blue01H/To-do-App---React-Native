4fa0e083103777ceccacfb6b93abc672b0e9fd4f
Blue01H <72617725+Blue01H@users.noreply.github.com>
2021-03-17 14:39:20 -0400

    Login Functionality

--- App.js
+++ App.js
@@ -1,92 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import Register from './src/pages/register';
+import Login from './src/pages/login';
 
-export default class App extends React.Component {
-  state={
-    email:"",
-    password:""
-  }
-  render(){
-    return (
-      <View style={styles.container}>
-      
-        <Text style={styles.logo}>F-List</Text>
-        
-        <View style={styles.inputView} >
-          
-          <TextInput  
-            style={styles.inputText}
-            placeholder="Email" 
-            placeholderTextColor="#003f5c"
-            onChangeText={text => this.setState({email:text})}/>
 
-        </View>
-
-        <View style={styles.inputView} >
-         
-          <TextInput  
-            secureTextEntry
-            style={styles.inputText}
-            placeholder="Password" 
-            placeholderTextColor="#003f5c"
-            onChangeText={text => this.setState({password:text})}/>
-
-        </View>
-
-        <TouchableOpacity style={styles.loginBtn}>
-          <Text style={styles.loginText}>Login</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity>
-          <Text style={styles.loginText}>Signup</Text>
-        </TouchableOpacity>
-
-      </View>
-    );
-  }
+export default function App() {
+  return (
+   <Login>
+   </Login>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#006aff',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  logo:{
-    fontWeight:"bold",
-    fontSize:50,
-    color:"#fff",
-    marginBottom:40
-  },
-  inputView:{
-    width:"80%",
-    backgroundColor:"#fff",
-    borderRadius:25,
-    height:50,
-    marginBottom:20,
-    justifyContent:"center",
-    padding:20
-  },
-  inputText:{
-    height:50,
-    color:"white"
-  },
-  forgot:{
-    color:"white",
-    fontSize:11
-  },
-  loginBtn:{
-    width:"80%",
-    backgroundColor:"#fb5b5a",
-    borderRadius:25,
-    height:50,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:40,
-    marginBottom:10
-  },
-  loginText:{
-    color:"white"
   }
-});
+})
